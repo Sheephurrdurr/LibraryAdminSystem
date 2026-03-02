f6f9c532a019d11e81bd0c5030c134f54becf22d
Nicolai <nicolai.hoiberg@hotmail.com>
2026-03-02 13:06:17 +0100

    Did some stuff.

--- Infrastructure/Data/LibraryContext.cs
+++ Infrastructure/Data/LibraryContext.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
 
 namespace Infrastructure.Data
 {
-    public class LibraryContext
+    public class LibraryContext : DbContext
     {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+
+        public LibraryContext() { }
+
+        public DbSet<Domain.Entities.Book> Books { get; set; }
+        public DbSet<Domain.Entities.Author> Authors { get; set; }
+        public DbSet<Domain.Entities.Borrower> Borrowers { get; set; }
+        public DbSet<Domain.Entities.Loan> Loans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.ConfigureSqLite();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BorrowerConfiguration());
+            modelBuilder.ApplyConfiguration(new LoanConfiguration());
+        }
     }
 }
